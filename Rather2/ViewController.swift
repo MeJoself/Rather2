9cf3087734600be325050669e88c49729dbe88bd
Jose Faustino <josefaustino.channel@gmail.com>
2022-05-09 13:22:52 -0600

    Initial Commit

---
+++ Rather2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rather2
+//
+//  Created by Jose Faustino on 5/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
