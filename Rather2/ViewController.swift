41159be44396000286a55d2d4ecc01f7ce52344d
Jose Faustino <josefaustino.channel@gmail.com>
2022-05-10 13:47:13 -0600

    MVP

--- Rather2/ViewController.swift
+++ Rather2/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstPicture: UIImageView!
     @IBOutlet weak var secPicture: UIImageView!
     
-    var phraseArray = ["Be in jail for five years", "Never be able to go out during the day", "Be forced to live the same day over and over again for a full year ", "Have universal respect", "Stay the age you are physically forever", "Get a paper cut everytime you turn a page", "Hunt and butcher your own meat"]
-   var phraseArrayTwo = ["Be in coma for a decade", "Or never be able to go out during the night", "Take 3 years off the end of your life", "Unlimted power", "Stay the way you are now financially forever", "Bite your tongue everytime you eat", "Never eat meat again"]
+    var phraseArray = ["Be in jail for five years", "Never be able to go out during the day", "Be forced to live the same day over and over again for a full year ", "Have universal respect", "Stay the age you are physically forever", "Get a paper cut everytime you turn a page", "Hunt and butcher your own meat", "Team up with the Avengers", "Spend a week in a forest", "Be able to read minds "]
+   var phraseArrayTwo = ["Be in coma for a decade", " never be able to go out during the night", "Take 3 years off the end of your life", "Unlimted power", "Stay the way you are now financially forever", "Bite your tongue everytime you eat", "Never eat meat again", "Team up with the Justice League", "Spend a night in a haunted house", "Be able to predic the future"]
     
-    var phaseArrayPic = ["Jail", "Sun Burn", "Groundhog Day", "Mike Tyson", "Never Ageing Keanu", "Heal me", "Shoot"]
-    var phaseArrayPicTwo = ["Coma", "Night", "Joe Recomend", "Palpatine", "Money", "Ouch", "No meat"]
+    var phaseArrayPic = ["Jail", "Sun Burn", "Groundhog Day", "Mike Tyson", "Never Ageing Keanu", "Heal me", "Shoot", "Avengers", "Forest", "Read minds"]
+    var phaseArrayPicTwo = ["Coma", "Night", "Joe Recomend", "Palpatine", "Money", "Ouch", "No meat", "Justice League", "House", "Simps"]
                             
                             
                             
